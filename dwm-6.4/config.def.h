24703046fe90dbb6abd56872dac31fdf76f5a744
Coltrane Willsey <cwillsey06@gmail.com>
2023-08-26 01:58:44 -0600

    dwm/feat: Apply `nordtheme` patch.

--- dwm-6.4/config.def.h
+++ dwm-6.4/config.def.h
@@ -6,15 +6,10 @@ static const unsigned int snap      = 8; /* snap pixel */
 static const int showbar            = 0; /* 0 means no bar */
 static const int topbar             = 0; /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { "#ECEFF4", "#2E3440", "#2E3440" },
+	[SchemeSel]  = { "#2E3440", "#88C0D0", "#2E3440" },
 };
 
 /* tagging */
