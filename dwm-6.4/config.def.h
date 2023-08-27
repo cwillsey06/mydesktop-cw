765ecf7803a365f5f1c463bfb0313f7ba9c76216
Coltrane Willsey <cwillsey06@gmail.com>
2023-08-27 02:59:43 -0600

    dwm/feat: Apply `col_nord8` to selected borders

--- dwm-6.4/config.def.h
+++ dwm-6.4/config.def.h
@@ -3,26 +3,27 @@
 #define SESSION_FILE "/tmp/dwm-session"
 
 /* appearance */
-static const unsigned int borderpx  = 2; /* border pixel of windows */
-static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
-static const unsigned int snap      = 8; /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
-static const int showbar            = 1; /* 0 means no bar */
-static const int topbar             = 0; /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenu_font[]      = "monospace:size=10";
-static const char col_nord0[]       = "#2E3440";
-static const char col_nord6[]       = "#ECEFF4";
-static const char col_nord8[]       = "#88C0D0";
-static const char *colors[][3]      = {
+static const Gap default_gap       = { .isgap = 1, .realgap = 10, .gappx = 10 };
+static const unsigned int borderpx = 2; /* border pixel of windows */
+static const unsigned int snap     = 8; /* snap pixel */
+static const int showsystray       = 1; /* 0 means no systray */
+static const int showbar           = 1; /* 0 means no bar */
+static const int topbar            = 0; /* 0 means bottom bar */
+static const char *fonts[]         = { "monospace:size=10" };
+static const char dmenu_font[]     = "monospace:size=10";
+static const char col_nord0[]      = "#2E3440";
+static const char col_nord6[]      = "#ECEFF4";
+static const char col_nord8[]      = "#88C0D0";
+static const char *colors[][3]     = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_nord6, col_nord0, col_nord0 },
-	[SchemeSel]  = { col_nord0, col_nord8, col_nord0 },
+	[SchemeSel]  = { col_nord0, col_nord8, col_nord8 },
 };
+
+static const unsigned int systraypinning = 0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft  = 0; /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2; /* systray spacing */
+static const int systraypinningfailfirst = 1; /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
