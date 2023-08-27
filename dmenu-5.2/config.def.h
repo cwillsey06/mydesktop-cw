6d2798221b5b67e763d5a63f06774ca82d94e015
Coltrane Willsey <cwillsey06@gmail.com>
2023-08-26 22:46:39 -0600

    dmenu/feat: dmenu now uses nord colors by default
    dmenu/refactor: Cleaned up `config.def.h`

--- dmenu-5.2/config.def.h
+++ dmenu-5.2/config.def.h
@@ -1,23 +1,33 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* default (nord) color names */
+static const char col_nord0[] = "#2E3440";
+static const char col_nord6[] = "#ECEFF4";
+static const char col_nord8[] = "#88C0D0";
+
+/* -b  option; if 0, dmenu appears at bottom */
+static int topbar = 1;
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-};
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
 
-/*
- * Characters not considered part of a word while deleting words
- * for example: " /?\"&[]"
- */
-static const char worddelimiters[] = " ";
+/* -p  option; prompt to the left of input field */
+static const char *prompt = NULL; 
+
+/* -nb,-nf,-sb,-sf options override default colors  */
+static const char *colors[SchemeLast][2] = {
+/*   scheme          fg         bg       */
+	[SchemeNorm] = { col_nord6, col_nord0 },
+	[SchemeSel]  = { col_nord0, col_nord8 },
+	[SchemeOut]  = { "#000000", "#00ffff" },
+};
+
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines = 0;
+
+/* Characters not considered part of a word while deleting words */
+static const char worddelimiters[] = " "; /* example: " /?\"&[]" */
+
